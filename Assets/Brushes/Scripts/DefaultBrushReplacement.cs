c2819037966985ca11767aba2c1dcdc799a45b98
dungtv0805 <dung.tv0805@gmail.com>
2020-12-20 08:01:07 +0700

    helloworld

---
+++ Assets/Brushes/Scripts/DefaultBrushReplacement.cs
@@ -0,0 +1,11 @@
+#if UNITY_EDITOR
+using UnityEditor.Tilemaps;
+using UnityEngine;
+
+[CustomGridBrush(true, true, true, "Default Brush")]
+public class DefaultBrushReplacement : GridBrush 
+{
+    // Just placeholder
+    // to show info text
+}
+#endif
